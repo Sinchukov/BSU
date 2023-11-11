8af27e51aba5ea53f12e1b4de6e0b0233f48e3cd
Sinchukov <maximsinch1941@gmail.com>
2023-11-11 16:48:48 +0300

    Fixed norm vector calc & residual vector calc

--- LAB 1/LAB 1/Gauss.h
+++ LAB 1/LAB 1/Gauss.h
@@ -6,8 +6,7 @@
 void input(double** A, double* b, int n);
 void print_system(double** A, double* b, int n);
 double* gauss(double** A, double* b, int n);
-void solve_auxiliary_system(double** A, double* x_prime, double*& x, int n);
 double* residual(double** A, double* b, double* x, int n);
 double calculateResidualNorm(double* residual, int n);
-
-
+double relativeError(double trueValue, double approximateValue);
+void solve_auxiliary_system(double** A, double*& x, int n);
--- LAB 1/LAB 1/LAB 1.cpp
+++ LAB 1/LAB 1/LAB 1.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main()
 {
-    double** A, * b, * x;
+    double** A, * b, * x, *roots2, *roots1, fullError = 0.0;
     int n;
     cout << "Input equation count: ";
     cin >> n;
-    A = new double* [n], b = new double[n];
+    A = new double* [n], b = new double[n], roots2 = new double[n], roots1 = new double[n];
     for (int i = 0; i < n; i++) {
         A[i] = new double[n];
     }
@@ -16,41 +16,23 @@ int main()
     x = gauss(A, b, n);
     for (int i = 0; i < n; i++) {
         cout << "x[" << i << "]=" << x[i] << endl;
-    }
-    double* roots1 = new double[n];
-    for (int i = 0; i < n; i++){
         roots1[i] = x[i];
     }
     cout << "residual vector: " << *residual(A, b, x, n) << endl;
     cout << "Norma: " << calculateResidualNorm(residual(A, b, x, n), n) << endl;
-    // Решаем вспомогательную систему для проверки
-    double* x_prime = new double[n];
+    // Решаем вспомогательную систему для проверки  
+    solve_auxiliary_system(A, x, n);
     for (int i = 0; i < n; i++) {
-        x_prime[i] = 1;
-    }
-    double* roots2 = new double[n];
-    solve_auxiliary_system(A, x_prime, x, n);
-    for (int i = 0; i < n; i++)
-    {
         roots2[i] = x[i];
-    }
-    cout << "Solution of Ax = Ax':" << endl;
-    for (int i = 0; i < n; i++) {
         cout << "roots2[" << i << "] = " << roots2[i] << endl;
+        fullError += relativeError(roots1[i], roots2[i]);
     }
-    double max_error = 0;
+    cout << "Relative Error: " << fullError << endl;
     for (int i = 0; i < n; i++) {
-        double error = abs((roots2[i] - roots1[i]) / roots1[i]);
-        if (error > max_error) {
-            max_error = error;
-        }
-    }
-    cout << "Relative Error: " << max_error << endl;
-    for (int i = 0; i < n; i++) {
-        b[i] = A[i][i] * x_prime[i];
+        b[i] = A[i][i];
         for (int j = 0; j < n; j++) {
             if (i != j) {
-                b[i] -= A[i][j] * x_prime[j];
+                b[i] -= A[i][j];
             }
         }
     }
@@ -58,6 +40,6 @@ int main()
     cout << "Norma: " << calculateResidualNorm(residual(A, b, x, n), n) << endl;
     cin.get(); cin.get();
 
-    delete[] roots1, roots2, x_prime;
+    delete[] roots1, roots2;
     return 0;
 }
