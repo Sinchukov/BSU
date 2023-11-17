cbadafc61099df84a5b28a498856eafa7c69171e
Sinchukov <maximsinch1941@gmail.com>
2023-11-18 02:08:13 +0300

    lab5 updated

--- LAB 2/LAB 2/Methods.cpp
+++ LAB 2/LAB 2/Methods.cpp
@@ -1,99 +1,23 @@
-#pragma once
 #include <iostream>
 #include <iomanip>
 #include <string>
 #include <sstream>
 #include <vector>
 #include <math.h>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <sstream>
+#include <math.h>
+
 using namespace std;
 
-void print(vector<double>);
-void print(vector<vector<double>>);
-void Nev(vector<double>&, vector<double>);
-void Jac(vector< vector<double>>&, vector<double>);
-void Jac2(vector< vector<double>>&, vector<double>, double);
-double fd1(vector<double>);
-double fd2(vector<double>, vector<double>);
 bool Gaus(vector<int>&, vector<double>&, vector<vector<double>>&, vector<double>&);
 bool GausSh(vector<int>&, vector<vector<double>>&, int, int);
 void In(vector<vector<double>>&, int);
 void print(vector<int>&, vector<vector<double>>&, int);
 int Max(vector<int>&, vector<vector<double>>&, int, int);
 void swap(vector<int>&, int, int);
-
-void print(vector<double> mas)
-{
-	for (int i = 0; i < mas.size(); i++)
-		cout << mas[i] << " ";
-	cout << endl << endl;
-}
-
-void print(vector<vector<double>> mas)
-{
-	int n = mas[0].size();
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-			cout << mas[i][j] << " ";
-		cout << endl;
-	}
-	cout << endl;
-}
-
-void Nev(vector<double>& F, vector<double> x)
-{
-	F[0] = 1.5 * pow(x[0], 3) - pow(x[1], 2) - 1;
-	F[1] = x[0] * pow(x[1], 3) - x[1] - 4;
-}
-
-void Jac(vector< vector<double>>& J, vector<double> x)
-{
-	J[0][0] = 4.5 * pow(x[0], 2);
-	J[0][1] = -2 * x[1];
-	J[1][0] = pow(x[1], 3);
-	J[1][1] = 3 * x[0] * pow(x[1], 2) - 1;
-}
-
-void Jac2(vector< vector<double>>& J, vector<double> x, double M)
-{
-	J[0][0] = ((1.5 * pow(x[0] + M * x[0], 3) - pow(x[1], 2) - 1) - (1.5 * pow(x[0], 3) - pow(x[1], 2) - 1)) / (M * x[0]);
-	J[0][1] = ((1.5 * pow(x[0], 3) - pow(x[1] + M * x[1], 2) - 1) - (1.5 * pow(x[0], 3) - pow(x[1], 2) - 1)) / (M * x[1]);
-	J[1][0] = (((x[0] + M * x[0]) * pow(x[1], 3) - x[1] - 4) - (x[0] * pow(x[1], 3) - x[1] - 4)) / (M * x[0]);
-	J[1][1] = ((x[0] * pow(x[1] + M * x[1], 3) - (x[1] + M * x[1]) - 4) - (x[0] * pow(x[1], 3) - x[1] - 4)) / (M * x[1]);
-}
-
-double fd1(vector<double> x)
-{
-	double x1 = 1.5 * pow(x[0], 3) - pow(x[1], 2) - 1;
-	double x2 = x[0] * pow(x[1], 3) - x[1] - 4;
-
-	if (abs(x1) > abs(x2))
-		return abs(x1);
-	else
-		return abs(x2);
-}
-
-double fd2(vector<double> x, vector<double> x1)
-{
-	int n = x.size();
-	double max = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (abs(x1[i]) < 1)
-		{
-			if (abs(x1[i] - x[i]) > max)
-				max = abs(x1[i] - x[i]);
-		}
-		else
-		{
-			if (abs((x1[i] - x[i]) / x1[i]) > max)
-				max = abs((x1[i] - x[i]) / x1[i]);
-		}
-	}
-
-	return max;
-}
-
 
 bool Gaus(vector<int>& p, vector<double>& x, vector<vector<double>>& J, vector<double>& F)
 {
@@ -147,16 +71,15 @@ bool GausSh(vector<int>& p, vector<vector<double>>& a, int n, int k)
 
 	double del = a[max][k];
 	cout << del << endl;
-	for (int j = k; j < n + 1; j++)
+	for (int j = k; j < n + 1; j++) {
 		a[max][j] /= del;
+	}
 	print(p, a, n);
 
-	for (int i = k + 1; i < n; i++)
-	{
+	for (int i = k + 1; i < n; i++){
 		del = a[p[i]][k];
 		cout << "del = " << del << endl;
-		for (int j = k; j < n + 1; j++)
-		{
+		for (int j = k; j < n + 1; j++){
 			cout << a[p[i]][j] << " -= " << a[p[k]][j] << " * " << del << " [ "<< (a[p[k]][j] * del) <<" ] "<< endl;
 			a[p[i]][j] -= (a[p[k]][j] * del);
 		}
@@ -166,20 +89,17 @@ bool GausSh(vector<int>& p, vector<vector<double>>& a, int n, int k)
 	return 1;
 }
 
-void In(vector<vector<double>>& a, int n)
-{
+void In(vector<vector<double>>& a, int n){
 	bool v = 0;
 	cout << "По умолч - 0 (для 4го), ввод - 1 : ";
 	cin >> v;
 	int i, j;
-	if (v)
-	{
+	if (v){
 		cout << "Введи коэффициенты (через пробел) и свободные члены ( после равно):" << endl;
 		string v;
 		cin.ignore();
 
-		for (i = 0; i < n; i++)
-		{
+		for (i = 0; i < n; i++){
 			cout << i + 1 << ") ";
 			getline(cin, v);
 			v += "= 0 = 0 = 0 = 0 = 0";
@@ -196,8 +116,7 @@ void In(vector<vector<double>>& a, int n)
 			str >> a[i][n];
 		}
 	}
-	else
-	{
+	else {
 		a[0][0] = 1; a[0][1] = 1; a[0][2] = 4; a[0][3] = 1; a[0][4] = 12;
 		a[1][0] = 4; a[1][1] = 2; a[1][2] = 1; a[1][3] = 2; a[1][4] = 12;
 		a[2][0] = 2; a[2][1] = 4; a[2][2] = 2; a[2][3] = 3; a[2][4] = 12;
@@ -205,11 +124,9 @@ void In(vector<vector<double>>& a, int n)
 	}
 }
 
-void print(vector<int>& p, vector<vector<double>>& a, int n)
-{
+void print(vector<int>& p, vector<vector<double>>& a, int n){
 	int i, j;
-	for (i = 0; i < n; i++)
-	{
+	for (i = 0; i < n; i++){
 		cout << p[i] << "   ";
 		for (j = 0; j < n; j++)
 			cout << setprecision(4) << fixed << setw(8) << a[i][j];
@@ -219,16 +136,12 @@ void print(vector<int>& p, vector<vector<double>>& a, int n)
 	cout << endl;
 }
 
-int Max(vector<int>& p, vector<vector<double>>& a, int n, int k)
-{
+int Max(vector<int>& p, vector<vector<double>>& a, int n, int k){
 	double max = 0;
 	int maxs = p[k];
 	cout << "  " << maxs << endl;
-
-	for (int i = k; i < n; i++)
-	{
-		if (abs(a[p[i]][k]) > max)
-		{
+	for (int i = k; i < n; i++){
+		if (abs(a[p[i]][k]) > max){
 			cout << abs(a[p[i]][k]) << " > " << max << endl;
 			max = abs(a[p[i]][k]);
 			maxs = p[i];
@@ -238,14 +151,14 @@ int Max(vector<int>& p, vector<vector<double>>& a, int n, int k)
 	return maxs;
 }
 
-void swap(vector<int>& p, int k1, int k2)
-{
-	int s1 = 0;
-	while (p[s1] != k1)
+void swap(vector<int>& p, int k1, int k2){
+	int s1 = 0, s2 = 0;;
+	while (p[s1] != k1) {
 		s1++;
-	int s2 = 0;
-	while (p[s2] != k2)
+	}
+	while (p[s2] != k2) {
 		s2++;
+	}
 	p[s1] = k2;
 	p[s2] = k1;
 }
